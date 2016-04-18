f59a8158255b4369401c0de4a371cbbe7506f711
Filipe Manuel Goncalves de Oliveira <filipemgoliveira94@gmail.com>
2016-04-18 14:31:42 +0100

    ola2

--- SRCR_Exercicio2/src/srcr_exercicio2/SRCR_Exercicio2.java
+++ SRCR_Exercicio2/src/srcr_exercicio2/SRCR_Exercicio2.java
@@ -16,6 +16,10 @@ public class SRCR_Exercicio2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String termString ="x";
+        //SICStus sp;
+        
+        
     }
     
 }
