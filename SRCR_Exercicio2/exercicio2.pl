b08e30cf707d936af2482336b37e804c2cb12ee4
Filipe Manuel Goncalves de Oliveira <filipemgoliveira94@gmail.com>
2016-04-22 11:33:49 +0100

    prolog1

--- SRCR_Exercicio2/exercicio2.pl
+++ SRCR_Exercicio2/exercicio2.pl
@@ -10,3 +10,14 @@
 :- dynamic servico/4.
 :- dynamic consulta/4.
 :- op( 900,xfy,'::' ).
+
+%#####################################################################
+% Extensao do predicado Utente: IdUt, Nome, Idade, Morada  -> {V,F,D}
+
+utente(1,"Joao",31,Vieira).
+utente(2,"Ana",22,Aveiro).
+utente(3,"Sofia",25,Cabeceiras).
+utente(4,"Carlos",18,Avintes).
+
+%#####################################################################
+% Extensao do predicado Servico: Serv, Descricao, Instituicao, Cidade  -> {V,F,D}
