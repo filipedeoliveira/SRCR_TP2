fae1ee05d9baf4b190e1990118534fb55f6876d9
Stéphane Fernandes <stephanefernandes@hotmail.com>
2016-04-25 15:03:51 +0100

    OLA :)

--- SRCR_Exercicio2/exercicio2.pl
+++ SRCR_Exercicio2/exercicio2.pl
@@ -17,58 +17,48 @@
 % Extensao do predicado Utente: IdUt, Nome, Idade, Morada  -> {V,F,D}
 
 utente(1,Joao,31,Vieira).
-%utente(2,Ana,22,Aveiro).
-utente(2,Ana,id_desc,Aveiro).
-%utente(3,Sofia,25,Cabeceiras).
-utente(3,Sofia,25,mor_desc).
+utente(2,Ana,desc,Aveiro).
+utente(3,Sofia,25,Cabeceiras).
 utente(4,Carlos,18,Avintes).
-utente(5,Bruno,34,Braga).
-%utente(6,Vitor,27,Porto).
-utente(6,Vitor,id_desc,Porto).
-%utente(7,Sandra,30,Faro).
-utente(7,Sandra,30,mor_desc).
+utente(5,Bruno,desc,Braga).
+utente(6,Vitor,27,Porto).
+utente(7,Sandra,30,desc).
 
--utente(IdUt,Nome,Idade,Morada):-
-					nao(utente(IdUt,Nome,Idade,Morada)),
-					nao(excepcao(utente(IdUt,Nome,Idade,Morada))).
+
+-utente(IdUt,Nome,Idade,Morada):-nao(utente(IdUt,Nome,Idade,Morada)), nao(excepcao(utente(IdUt,Nome,Idade,Morada))).
 
 %#####################################################################
-% Extensao do predicado Servico: Serv, Descricao, Instituicao, Cidade  -> {V,F,D}
+% Extensao do predicado Servico: Servico, Descricao, Instituicao, Cidade  -> {V,F,D}
 
-%servico(1001,psiquiatria,hospitalBraga,Braga).
-servico(1001,psiquiatria,hospitalBraga,cid_desc).
+
+servico(1001,psiquiatria,hospitalBraga,desc).
 servico(1002,neurologia,hospitalPorto,Porto).
 servico(1003,cardiologia,hospitalAgra,Braga).
 servico(1001,psiquiatria,hospitalPorto,Porto).
-servico(1004,oncologia,hospitalBraga,Braga).
-%servico(1003,cardiologia,hospitalPorto,Porto).
-servico(1003,cardiologia,hospitalPorto,cid_desc).
+servico(1004,oncologia,hospitalBraga,desc).
+servico(1003,cardiologia,hospitalPorto,desc).
 servico(1005,fisioterapia,hospitalFaro,Faro).
-%servico(1003,cardiologia,hospitalFaro,Faro).
-servico(1003,cardiologia,hospitalFaro,cid_desc).
+servico(1003,cardiologia,hospitalFaro,Braga).
 servico(1001,psiquiatria,hospitalAgra,Agra).
 
--servico(Serv,Descricao,Instituicao,Cidade):-
-						nao(servico(Serv,Descricao,Instituicao,Cidade)),
-						nao(excepcao(servico(Serv,Descricao,Instituicao,Cidade))).
+-servico(Servico,Descricao,Instituicao,Cidade):- nao(servico(Servico,Descricao,Instituicao,Cidade)),
+						nao(excepcao(servico(Servico,Descricao,Instituicao,Cidade))).
 
 %#####################################################################
-%extensao do predicado consulta: Data, IdUt, Serv, Custo ->{ V, F, D }
+%extensao do predicado consulta: Data, Id, Serv, Custo ->{ V, F, D }
 consulta(data(10,08,2014),1,1001,40).
-%consulta(data(08,03,2013),2,1002,30).
 consulta(data(08,03,2013),2,1002,desc).
 consulta(data(27,02,2016),3,1005,35).
 consulta(data(17,11,2013),4,1004,50).
 consulta(data(30,12,2015),5,1005,35).
 consulta(data(14,06,2013),6,1002,30).
-%consulta(data(07,09,2015),7,1003,60).
 consulta(data(07,09,2015),7,1003,desc).
 consulta(data(5,08,2014),3,1001,40).
 
 
--consulta(Data,IdUt,Serv,Custo):-
-						nao(consulta(Data,IdUt,Serv,Custo)),
-						nao(excepcao(consulta(Data,IdUt,Serv,Custo))).
+-consulta(Data,Id,Serv,Custo):-
+						nao(consulta(Data,Id,Serv,Custo)),
+						nao(excepcao(consulta(Data,Id,Serv,Custo))).
 
 %####################################################################
 % Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
@@ -87,11 +77,102 @@ demo( Questao,desconhecido ) :-
 nao( Questao ) :- Questao, !, fail.
 nao( Questao ).
 
+solucoes( X,Y,Z ) :- findall( X,Y,Z ).
 
+comprimento( S,N ) :- length( S,N ).
+	
 % ##########Excepcoes:###############################################
 
 excepcao(utente(IdUt, Nome, Idade, Morada)):- Idade==desc.
 excepcao(utente(IdUt, Nome, Idade, Morada)):- Morada==desc.
-excepcao(servico(Serv, Descricao, Instituicao, Cidade)):- Cidade==desc.
-excepcao(consulta(Data, IdUt, Serv, Custo)):- Custo==desc.
+
+% Nao se sabe se a Ana tem 25 ou 33 anos
+excepcao(utente(2,Ana,25,Aveiro)).
+excepcao(utente(2,Ana,33,Aveiro)).
+% Nao se sabe se o Bruno tem 16 ou 26 anos
+excepcao(utente(5,Bruno,16,Braga)).
+excepcao(utente(5,Bruno,26,Braga)).
+% Nao se sabe a Morada da Sandra podendo ser Faro ou Beja
+excepcao(utente(7,Sandra,30,Faro)).
+excepcao(utente(7,Sandra,30,Beja)).
+
+excepcao(servico(Servico, Descricao, Instituicao, Cidade)):- Cidade==desc.
+excepcao(servico(1001,psiquiatria,hospitalBraga,Gaia)).
+excepcao(servico(1001,psiquiatria,hospitalBraga,Funchal)).
+excepcao(servico(1004,oncologia,hospitalBraga,Lisboa)).
+excepcao(servico(1004,oncologia,hospitalBraga,Aveiro)).
+excepcao(servico(1003,cardiologia,hospitalPorto,Agra)).
+excepcao(servico(1003,cardiologia,hospitalPorto,Coimbra)).
+
+excepcao(consulta(Data, Id, Serv, Custo)):- Custo==desc.
+excepcao(consulta(data(08,03,2013),2,1002,35)).
+excepcao(consulta(data(08,03,2013),2,1002,45)).
+excepcao(consulta(data(07,09,2015),7,1003,60)).
+excepcao(consulta(data(07,09,2015),7,1003,55)).
+
+%#######################Mecanismos de raciocinio#####################
+
+%"Getters"
+IdUt(utente(IdUt, Nome, Idade, Morada),IdUt).
+Nome(utente(IdUt, Nome, Idade, Morada),Nome).
+Idade(utente(IdUt, Nome, Idade, Morada),Idade).
+Morada(utente(IdUt, Nome, Idade, Morada),Morada).
+
+Servico((Servico, Descricao, Instituicao, Cidade),Servico).
+Descricao((Servico, Descricao, Instituicao, Cidade),Descricao).
+Instituicao((Servico, Descricao, Instituicao, Cidade),Instituicao).
+Cidade((Servico, Descricao, Instituicao, Cidade),Cidade).
+
+Data((Data, Id, Serv, Custo),Data).
+Id((Data, Id, Serv, Custo),Id).
+Serc((Data, Id, Serv, Custo),Serv).
+Custo((Data, Id, Serv, Custo),Custo).
+
+%Outros Mecanismos
+
+%lista dos servicos que um hospital disponibiliza
+listaServicos(Instituicao, Lista):- findall(Descricao,servico(_,Descricao,Instituicao,_),Lista).
+%(..se for preciso faz-se mais alguns q sejam interessantes...)
+
+
+############################################################################################
+########################### VER BEM ########################################################
+############################################################################################
+% Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
+
+demo([],[]).
+demo([Questao|Questoes],[verdadeiro|S]) :-
+    Questao,
+    demo(Questoes,S).
+
+demo([Questao|Questoes],[falso|S]) :-
+    -Questao,
+    demo(Questoes,S).
+
+demo([Questao|Questoes],[desconhecido|S]) :-
+    nao( Questao ),
+    nao( -Questao ),
+    demo(Questoes,S).
+
+
+demonstr(Questao,verdadeiro):-
+Questao.
+
+demonstr(Questao,falso):-
+-Questao.
+
+demonstr(Questao,desconhecido):-
+nao(Questao),
+nao(-Questao).
+
+############################################################################################
+############################################################################################
+
+
+
+
+
+
+
+
 
